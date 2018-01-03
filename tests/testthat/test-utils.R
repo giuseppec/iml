4b72e1e9c2cccb34bada498c682ef59b7f762b52
Christoph Molnar <christoph.molnar@gmail.com>
2018-01-03 10:26:09 +0100

    adapts tests for get.feature.type;

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -4,10 +4,11 @@ context('Test utility functions')
 test_that('get.feature.type function works', {
   
   f1 = c('a', 'b')
-  expect_equal(get.feature.type(f1), 'categorical')
+  expect_equal(get.feature.type(class(f1)), c('character'='categorical'))
   expect_error(get.feature.type(data.frame(f1)))
-  expect_equal(get.feature.type(factor(f1)), 'categorical')
+  expect_equal(get.feature.type(class(factor(f1))), c('factor'='categorical'))
   
   f2 = 1:10
-  expect_equal(get.feature.type(f2), 'numerical')
+  expect_equal(get.feature.type(c(class(f1), class(f2))), 
+    c('character'='categorical', 'integer'='numerical'))
 })
