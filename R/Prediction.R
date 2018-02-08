abdfb03c84116145e60a2ad368f2b9368fe66bd9
Christoph Molnar <christoph.molnar@gmail.com>
2018-02-08 17:42:50 +0100

    adds caret and mlr

--- R/Prediction.R
+++ R/Prediction.R
@@ -3,6 +3,7 @@
 #' @param object function, mlr WrappedObject, S3 class with predict function, or caret train object
 #' @param class In case of classification, class specifies the class for which to predict the probability. 
 #' By default the first class in the prediction (first column) is chosen. 
+#' @import mlr
 #' @return object of type Prediction
 prediction.model = function(object, class = NULL, predict.args = NULL){
   assert_vector(class, len=1, null.ok=TRUE)
