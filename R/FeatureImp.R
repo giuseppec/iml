f51ebc98cda8d9c31cb7a2305cbf99a1e42528fb
Christoph Molnar <christoph.molnar@gmail.com>
2018-02-21 16:00:27 +0100

    reworks FeatureImp docs

--- R/FeatureImp.R
+++ R/FeatureImp.R
@@ -1,54 +1,84 @@
 #' Feature importance
 #' 
 #' @description 
-#' makeFeatureImp() computes feature importances for machine learning models. 
-#' The importance of a feature is the factor by which the model's prediction error increases when the feature is shuffled. 
+#' \code{FeatureImp} computes feature importances for prediction models.
+#' Importance is measured as the factor by which the model's prediction error increases when the feature is shuffled. 
 #' 
-#' @details
+#' @format \code{\link{R6Class}} object.
+#' @name FeatureImp
+#' 
+#' @section Usage:
+#' \preformatted{
+#' imp = FeatureImp$new(predictor, data, y, loss, method = "shuffle", run = TRUE)
+#' 
+#' plot(imp)
+#' imp$data()
+#' imp$tree
+#' print(imp)
+#' } 
+#' 
+#' @section Arguments:
+#' 
+#' For FeatureImp$new():
+#' \describe{
+#' \item{predictor}{Object of type \code{Predictor}. See \link{Predictor}}
+#' \item{data}{data.frame with the data for the prediction model.}
+#' \item{run}{logical. Should the Interpretation method be run?}
+#' \item{loss}{The loss function. A string (e.g. "ce" for classification or "mse") or a function. See Details.}
+#' \item{method}{Either "shuffle" or "cartesian". See Details.}
+#' \item{y}{The vector or data.frame with the actual target values associated with X.}
+#' }
+#' 
+#' @section Details:
 #' Read the Interpretable Machine Learning book to learn more about feature importance: 
-#' \url{https://christophm.github.io/interpretable-ml-book/permutation-feature-importance.html}
+#' \url{https://christophm.github.io/interpretable-ml-book/feature-importance.html}
 #' 
 #' Two permutation schemes are implemented: 
 #' \itemize{
 #' \item shuffle: A simple shuffling of the feature values, yielding n perturbed instances per feature (faster)
 #' \item cartesian: Matching every instance with the feature value of all other instances, yielding n x (n-1) perturbed instances per feature (slow)
 #' }
-#' The loss function can be either specified via a string, or by handing a function to \code{makeFeatureImp()}.
+#' The loss function can be either specified via a string, or by handing a function to \code{FeatureImp()}.
 #' Using the string is a shortcut to using loss functions from the \code{Metrics} package. 
 #' See \code{library(help = "Metrics")} to get a list of functions. 
 #' Only use functions that return a single performance value, not a vector. 
 #' You can also provide a function directly. It has to take the actual value as its first argument and the prediction as its second. 
 #' 
 #' 
-#' @param loss The loss function. A string (e.g. "ce" for classification or "mse") or a function. See Details.
-#' @param method Either "shuffle" or "cartesian". See Details. 
-#' @param y The vector or data.frame with the actual target values associated with X.
-#' @return 
-#' An FeatureImp object (R6). Its methods and variables can be accessed with the \code{$}-operator:
+#' @section Fields:
+#' \describe{
 #' \item{error.original}{The loss of the model before perturbing features.}
 #' \item{loss}{The loss function. Can also be applied to data: \code{object$loss(actual, predicted)}}
+#' }
+#' 
+#' @section Methods:
+#' \describe{
 #' \item{data()}{method to extract the results of the feature importance computation.
 #' Returns a data.frame with importance and permutation error measurements per feature.}
 #' \item{plot()}{method to plot the feature importances. See \link{plot.FeatureImp}}
-#' @template args_internal_methods
+#' \item{\code{run()}}{[internal] method to run the interpretability method. Use \code{obj$run(force = TRUE)} to force a rerun.}
+#' General R6 methods
+#' \item{\code{clone()}}{[internal] method to clone the R6 object.}
+#' \item{\code{initialize()}}{[internal] method to initialize the R6 object.}
+#' }
 #' 
 #' @references 
 #' Fisher, A., Rudin, C., and Dominici, F. (2018). Model Class Reliance: Variable Importance Measures for any Machine Learning Model Class, from the "Rashomon" Perspective. Retrieved from http://arxiv.org/abs/1801.01489
-#' @export
+#' 
 #' @import Metrics
-#' @template args_experiment_wrap
 #' @examples
 #' # We train a tree on the Boston dataset:
 #' if (require("rpart")) {
 #' data("Boston", package  = "MASS")
-#' mod = rpart(medv ~ ., data = Boston)
+#' tree = rpart(medv ~ ., data = Boston)
+#' mod = makePredictor(tree)
 #' 
 #' # Compute the individual conditional expectations for the first feature
 #' X = Boston[-which(names(Boston) == "medv")]
 #' y = Boston$medv
 #' 
 #' # Compute feature importances as the performance drop in mean absolute error
-#' imp = makeFeatureImp(mod, X, y, loss = "mae")
+#' imp = FeatureImp$new(mod, X, y, loss = "mae")
 #' 
 #' # Plot the results directly
 #' plot(imp)
@@ -64,13 +94,14 @@
 #' ggplot(imp.dat, aes(x = ..feature, y = importance)) + geom_point() + 
 #' theme_bw()
 #' 
-#' # makeFeatureImp() also works with multiclass classification. 
+#' # FeatureImp also works with multiclass classification. 
 #' # In this case, the importance measurement regards all classes
-#' mod = rpart(Species ~ ., data= iris)
+#' tree = rpart(Species ~ ., data= iris)
+#' mod = makePredictor(tree, predict.args = list(type = "prob")) 
 #' X = iris[-which(names(iris) == "Species")]
 #' y = iris$Species
 #' # For some models we have to specify additional arguments for the predict function
-#' imp = makeFeatureImp(mod, X, y, loss = "ce", predict.args = list(type = "prob"))
+#' imp = FeatureImp$new(mod, X, y, loss = "ce")
 #' plot(imp)
 #' # Here we encounter the special case that the machine learning model perfectly predicts
 #' # The importance becomes infinite
@@ -78,8 +109,8 @@
 #' 
 #' # For multiclass classification models, you can choose to only compute performance for one class. 
 #' # Make sure to adapt y
-#' imp = makeFeatureImp(mod, X, y == "virginica", class = 3, loss = "ce", 
-#'     predict.args = list(type = "prob"))
+#' mod = makePredictor(tree, predict.args = list(type = "prob"), class = 3) 
+#' imp = FeatureImp$new(mod, X, y == "virginica",loss = "ce")
 #' plot(imp)
 #' }
 NULL
@@ -181,7 +212,7 @@ FeatureImp = R6::R6Class("FeatureImp",
 #' 
 #' plot.FeatureImp() plots the feature importance results of an FeatureImp object.
 #' 
-#' For examples see \link{makeFeatureImp}
+#' For examples see \link{FeatureImp}
 #' @param x The feature importance. An FeatureImp R6 object
 #' @param sort logical. Should the features be sorted in descending order? Defaults to TRUE.
 #' @param ... Further arguments for the objects plot function
@@ -189,7 +220,7 @@ FeatureImp = R6::R6Class("FeatureImp",
 #' @export
 #' @importFrom dplyr group_by_
 #' @seealso 
-#' \link{makeFeatureImp}
+#' \link{FeatureImp}
 plot.FeatureImp = function(x, sort = TRUE, ...) {
   x$plot(sort = sort, ...)
 }
