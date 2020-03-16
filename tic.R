d5373863f1c45f2fabf7fe7792ce502e2a482a14
pat-s <patrick.schratz@gmail.com>
2020-03-16 14:10:13 +0100

    install keras

--- tic.R
+++ tic.R
@@ -1,6 +1,9 @@
 # installs dependencies, runs R CMD check, runs covr::codecov()
 do_package_checks()
 
+get_stage("install") %>% 
+  add_code_step(keras::install_keras())
+
 if (ci_on_ghactions()) {
   # creates pkgdown site and pushes to gh-pages branch
   do_pkgdown()
