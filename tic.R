93e8649036026d48a8c6c5ede03bf188c092fb29
pat-s <patrick.schratz@gmail.com>
2020-06-25 16:33:28 +0200

    try latest python version with keras

--- tic.R
+++ tic.R
@@ -3,7 +3,6 @@ do_package_checks()
 
 get_stage("install") %>% 
   add_code_step(reticulate::install_miniconda()) %>% 
-  add_code_step(reticulate::conda_create('r-reticulate', packages = c('python==3.6.9'))) %>% 
   add_code_step(keras::install_keras())
 
 if (ci_on_ghactions()) {
