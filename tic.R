dcabe538715588c5f2c4de50e2eb543c335d9a9b
pat-s <patrick.schratz@gmail.com>
2020-06-27 08:27:47 +0200

    tic: only build pkgdown once

--- tic.R
+++ tic.R
@@ -5,7 +5,7 @@ get_stage("install") %>%
   add_code_step(reticulate::install_miniconda()) %>% 
   add_code_step(keras::install_keras())
 
-if (ci_on_ghactions()) {
+if (ci_on_ghactions() && ci_has_env("BUILD_PKGDOWN")) {
   # creates pkgdown site and pushes to gh-pages branch
   do_pkgdown()
 }
