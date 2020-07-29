59f2af74396a465e6aa1a926c5b6b4b1f5cec795
pat-s <patrick.schratz@gmail.com>
2020-07-29 15:47:53 +0200

    - `FeatureImp()`: robustify condition checking for only zeros in the calculated error

--- R/FeatureImp.R
+++ R/FeatureImp.R
@@ -162,7 +162,7 @@ FeatureImp <- R6::R6Class("FeatureImp",
       predicted <- private$run.prediction(private$sampler$X)[[1]]
       # Assuring that levels are the same
       self$original.error <- loss(actual, predicted)
-      if (self$original.error == 0 & self$compare == "ratio") {
+      if (all(self$original.error == 0) && self$compare == "ratio") {
         warning("Model error is 0, switching from compare='ratio' to compare='difference'")
         self$compare <- "difference"
       }
