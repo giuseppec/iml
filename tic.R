beda77434744acf353e1c71a3642a8bf33c7c752
pat-s <patrick.schratz@gmail.com>
2021-03-27 16:27:06 +0100

    try installing tensorflow explicitly

--- tic.R
+++ tic.R
@@ -3,6 +3,7 @@ do_package_checks()
 
 get_stage("install") %>% 
   add_code_step(reticulate::install_miniconda()) %>% 
+  add_code_step(tensorflow::install_tensorflow()) %>% 
   add_code_step(keras::install_keras())
 
 if (ci_on_ghactions() && ci_has_env("BUILD_PKGDOWN")) {
